e68df17657b32f994f0365433d6f480955b83c16
Pustam Rai <github.pustam@gmail.com>
2025-05-09 10:31:48 +0545

    update home component for responsive design and improved layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,9 +12,10 @@ function Home() {
         keywords="Pustam Rai, MERN Stack Developer, Full Stack Developer, React Developer, Node.js, MongoDB, Express.js, Portfolio, Web Developer Nepal"
         image="https://www.pustamrai.com.np/logo2.jpg"
       />
-      
+
       <div className="py-10 p-10 md:px-44 min-h-screen">
-        <div className="flex justify-center">
+        {/* mobile view */}
+        <div className="flex justify-center lg:hidden">
           <img
             src={ProfilePic}
             alt="profilePic"
@@ -22,44 +23,103 @@ function Home() {
           />
         </div>
 
-        <h1 className="py-6 text-3xl md:text-4xl font-bold text-gray-200 mt-4">
-          Hi, I'm <span className="text-blue-600">Pustam Rai</span>
-        </h1>
-        <h2 className="text-xl md:text-2xl font-semibold bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent mt-2">
-          MERN Stack Developer
-        </h2>
+        <div className="lg:hidden">
+          <h1 className="py-6 text-3xl md:text-4xl font-bold text-gray-200 mt-4">
+            Hi, I'm <span className="text-blue-600">Pustam Rai</span>
+          </h1>
+          <h2 className="text-xl md:text-2xl font-semibold bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent mt-2">
+            MERN Stack Developer
+          </h2>
 
-        <p className="text-gray-200 mt-4 max-w-lg">
-          I'm passionate about building high-performance web applications using
-          MongoDB, Express, React, and Node.js. I love solving problems and
-          turning ideas into reality.
-        </p>
+          <p className="text-gray-200 mt-4 max-w-lg">
+            I'm passionate about building high-performance web applications
+            using MongoDB, Express, React, and Node.js. I love solving problems
+            and turning ideas into reality.
+          </p>
 
-        <div className="mt-6 flex items-center space-x-6">
-          <a
-            href="https://www.linkedin.com/in/pustamrai"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 text-3xl rounded-full shadow-lg hover:text-blue-300 transition-all duration-150 ease-in cursor-pointer"
-          >
-            <FaLinkedin />
-          </a>
-          <a
-            href="https://github.com/PustamRai"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-100 text-3xl rounded-full shadow-lg hover:text-gray-500 transition-all duration-150 ease-in cursor-pointer"
-          >
-            <FaGithub />
-          </a>
-          <a
-            href="mailto:pustamrai.info@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-red-400 text-3xl rounded-full shadow-lg hover:text-red-500 transition-all duration-150 ease-in cursor-pointer"
-          >
-            <FaEnvelope />
-          </a>
+          <div className="mt-6 flex items-center space-x-6">
+            <a
+              href="https://www.linkedin.com/in/pustamrai"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 text-3xl rounded-full shadow-lg hover:text-blue-300 transition-all duration-150 ease-in cursor-pointer"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://github.com/PustamRai"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-100 text-3xl rounded-full shadow-lg hover:text-gray-500 transition-all duration-150 ease-in cursor-pointer"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="mailto:pustamrai.info@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-red-400 text-3xl rounded-full shadow-lg hover:text-red-500 transition-all duration-150 ease-in cursor-pointer"
+            >
+              <FaEnvelope />
+            </a>
+          </div>
+        </div>
+
+        {/* web view */}
+        <div className="hidden lg:block">
+          <div className="flex gap-12 justify-center items-center ">
+            {/* left */}
+            <div className="flex justify-center">
+              <img
+                src={ProfilePic}
+                alt="profilePic"
+                className="h-[540px] rounded-sm object-cover shadow-lg"
+              />
+            </div>
+
+            {/* right */}
+            <div className="">
+              <h1 className="py-6 text-3xl md:text-4xl font-bold text-gray-200 mt-4">
+                Hi, I'm <span className="text-blue-600">Pustam Rai</span>
+              </h1>
+              <h2 className="text-xl md:text-2xl font-semibold bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent mt-2">
+                MERN Stack Developer
+              </h2>
+
+              <p className="text-gray-200 mt-4 max-w-lg">
+                I'm passionate about building high-performance web applications
+                using MongoDB, Express, React, and Node.js. I love solving
+                problems and turning ideas into reality.
+              </p>
+
+              <div className="mt-6 flex items-center space-x-6">
+                <a
+                  href="https://www.linkedin.com/in/pustamrai"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 text-3xl rounded-full shadow-lg hover:text-blue-300 transition-all duration-150 ease-in cursor-pointer"
+                >
+                  <FaLinkedin />
+                </a>
+                <a
+                  href="https://github.com/PustamRai"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-gray-100 text-3xl rounded-full shadow-lg hover:text-gray-500 transition-all duration-150 ease-in cursor-pointer"
+                >
+                  <FaGithub />
+                </a>
+                <a
+                  href="mailto:pustamrai.info@gmail.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-red-400 text-3xl rounded-full shadow-lg hover:text-red-500 transition-all duration-150 ease-in cursor-pointer"
+                >
+                  <FaEnvelope />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
