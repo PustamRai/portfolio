5414d19a47de4267d9732432dcaeb3fafc59d755
Pustam Rai <github.pustam@gmail.com>
2025-05-09 09:36:11 +0545

    update projects page layout and styling for improved visual consistency and user experience

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -16,11 +16,12 @@ function Projects() {
 
       <h2 className="flex justify-center py-4 text-2xl mb-">My recent work</h2>
 
+      {/* card section */}
       <div className="md:w-3xl grid grid-cols-1 md:grid-cols-2 gap-6">
         {projects.map((project, idx) => (
           <div
             key={idx}
-            className=" border-3 border-gray-800  rounded-2xl  overflow-hidden shadow-sm hover:shadow-blue-300 transition-all duration-150 ease-in"
+            className=" border-3 border-gray-800  rounded-4xl  overflow-hidden shadow-sm hover:shadow-blue-300 transition-all duration-150 ease-in"
           >
             <img
               src={project.image}
@@ -28,6 +29,10 @@ function Projects() {
               className="h-52 w-full object-fill"
             />
 
+          {/*  */}
+          <div className="bg-gray-300 text-black py-2 backdrop-blur-sm h-full">
+
+          
             <div className="px-4 mt-3">
               <h2 className="font-bold text-2xl mb-2">{project.title}</h2>
               <p className="text-[15px]">{project.description}</p>
@@ -37,7 +42,7 @@ function Projects() {
               {project.technologies.map((technology, idx) => (
                 <span
                   key={idx}
-                  className="bg-green-500 rounded-sm p-1 text-[12px]"
+                  className="bg-black text-white p-1 text-[12px] rounded-sm"
                 >
                   {technology}
                 </span>
@@ -45,29 +50,31 @@ function Projects() {
             </div>
 
             <div className="flex justify-between items-center px-4 p-2 mt-3">
-              <div className="flex justify-center items-center gap-2 hover:text-gray-300">
+              <div className="flex justify-center items-center gap-2 hover:text-gray-300 bg-black rounded-4xl text-black py-4 px-6 hover:bg-blue-900 transition-colors cursor-pointer">
                 <a
                   href={project.github}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="flex justify-center items-center gap-2"
+                  className="flex justify-center items-center gap-2 text-white"
                 >
-                  <BsGithub size={20} className="transition-all duration-150 ease-in cursor-pointer" />
+                  <BsGithub size={20} className="transition-all duration-150 ease-in  text-bold" />
                   <p className="text-sm">view code</p>
                 </a>
               </div>
 
-              <div className="flex justify-center items-center gap-2 hover:text-blue-300">
+              <div className="flex justify-center items-center gap-2 hover:text-gray-300 bg-black rounded-4xl text-black py-4 px-6 hover:bg-blue-900 transition-colors cursor-pointer">
                 <a
                   href={project.link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="flex justify-center items-center gap-2"
+                  className="flex justify-center items-center gap-2 text-white"
                 >
                   <p className="text-sm">live demo</p>
-                  <TbExternalLink size={20} className="transition-all duration-150 ease-in cursor-pointer" />
+                  <TbExternalLink size={20} className="transition-all duration-150 ease-in" />
                 </a>
               </div>
+            </div>
+
             </div>
           </div>
         ))}
