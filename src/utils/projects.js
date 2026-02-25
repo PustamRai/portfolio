45cc69b09fbb08693876e0c2e3ff9b8444043387
Pustam Rai <github.pustam@gmail.com>
2026-02-25 09:53:59 +0545

    remove todo app project

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -22,13 +22,13 @@ export const projects = [
     link: "https://dokan-liart.vercel.app",
     github: "https://github.com/PustamRai/dokan",
   },
-  {
-    title: "Todo App",
-    image: TodoAppImage,
-    description:
-      "A simple and efficient task management tool built with React and Tailwind CSS, Nodejs and MongoDB. It allows users to add, update, delete and complete tasks with an intuitive interface. The app helps users stay organized by categorizing tasks into active and completed sections.",
-    technologies: ["React", "Tailwind CSS", "Node.js", "MongoDB"],
-    link: "https://todogoflow.vercel.app",
-    github: "https://github.com/PustamRai/Todolist",
-  },
+  // {
+  //   title: "Todo App",
+  //   image: TodoAppImage,
+  //   description:
+  //     "A simple and efficient task management tool built with React and Tailwind CSS, Nodejs and MongoDB. It allows users to add, update, delete and complete tasks with an intuitive interface. The app helps users stay organized by categorizing tasks into active and completed sections.",
+  //   technologies: ["React", "Tailwind CSS", "Node.js", "MongoDB"],
+  //   link: "https://todogoflow.vercel.app",
+  //   github: "https://github.com/PustamRai/Todolist",
+  // },
 ];
